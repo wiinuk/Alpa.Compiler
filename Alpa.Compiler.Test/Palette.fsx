53de75359dd8465881a8fbc8736f5ca6e3e82844
wiinukegara <wii.nukegara@gmail.com>
2016-07-19 20:26:00 +0900

    sync

--- Alpa.Compiler.Test/Palette.fsx
+++ Alpa.Compiler.Test/Palette.fsx
@@ -28,28 +28,42 @@ let raiseReadExn f = Printf.kprintf (ReadException >> raise) f
 let streamOfString xs = { Items = (xs + "").ToCharArray(); Index = 0 }
 let runString r xs = r <| streamOfString xs
 
-let satisfy predicate xs =
-    xs.Index < xs.Items.Length &&
+let canRead xs = xs.Index < xs.Items.Length
+
+let skipSatisfy predicate xs =
+    canRead xs &&
     predicate xs.Items.[xs.Index] &&
     (
         xs.Index <- xs.Index + 1
         true
     )
 
+let satisfy predicate xs =
+    if canRead xs then
+        let c = xs.Items.[xs.Index]
+        if predicate c then
+            xs.Index <- xs.Index + 1
+            int c
+        else -1
+    else -1
+
 let readTrivia xs =
-    let rec aux xs = if satisfy Char.IsWhiteSpace xs then aux xs else ()
+    let rec aux xs = if skipSatisfy Char.IsWhiteSpace xs then aux xs else ()
     aux xs
 
 let readCharT c xs =
     readTrivia xs
-    if satisfy ((=) c) xs then () else raiseReadExn "readCharT %c" c
+    if skipSatisfy ((=) c) xs then () else raiseReadExn "readCharT %c" c
     readTrivia xs
+
+let readChar c xs = satisfy ((=) c) xs
+let readAny xs = satisfy (fun _ -> true) xs
 
 let readStringT s xs =
     readTrivia xs
     let rec aux i =
         if String.length s = i then ()
-        elif satisfy ((=) s.[i]) xs then aux (i+1)
+        elif skipSatisfy ((=) s.[i]) xs then aux (i+1)
         else raiseReadExn "readStringT %A" s
     aux 0
     readTrivia xs
@@ -65,9 +79,17 @@ let readTuple3 r1 r2 r3 xs =
     x1, x2, x3
 
 let readTable rs xs =
+    let i = xs.Index
     let rec aux = function
         | [] -> raiseReadExn "readTable"
-        | (r,v)::rs -> try ignore(r xs); v with _ -> aux rs
+        | (r,v)::rs ->
+            try
+                ignore(r xs)
+                v
+            with 
+            | ReadException _ ->
+                xs.Index <- i
+                aux rs
     aux rs
 
 let readBool =
@@ -77,31 +99,73 @@ let readBool =
     ]
 
 let readIntT xs =
-    
+    let readAsciiDigitOrM1 xs = satisfy (fun c -> '0' <= c && c <= '9') xs
+    let toInt c = int c - int '0'
 
-let readHms xs =
-    let h = readIntT xs
-    readCharT ':' xs
-    let m = readIntT xs
-    readCharT ':' xs
-    let s = readIntT xs
-    h, m, s
+    readTrivia xs
+    let sign =
+        let c = satisfy (fun c -> c = '-' || c = '+') xs 
+        if char c = '-' then -1 else 1
+
+    match readAsciiDigitOrM1 xs with
+    | -1 -> raiseReadExn "readIntT"
+    | c ->
+        let n = sign * toInt c
+        let rec aux n xs =
+            match readAsciiDigitOrM1 xs with
+            | -1 -> n
+            | c ->
+                let n = Checked.(+) (Checked.(*) 10 n) (toInt c)
+                aux n xs
+        try
+            let n = aux n xs
+            readTrivia xs
+            n
+        with
+        | :? OverflowException -> raiseReadExn "readIntT overflow"
+
+let readStringLiteral xs =
+    readTrivia xs
+    if readChar '"' xs = -1 then raiseReadExn "readStringLiteral"
+    else
+
+    let buf = System.Text.StringBuilder()
+    let rec aux () =
+        match readAny xs with
+        | -1 -> raiseReadExn "readStringLiteral"
+        | c ->
+            match char c with
+            | '"' -> ()
+            | '\\' ->
+                match readAny xs with
+                | -1 -> raiseReadExn "readStringLiteral"
+                | c ->
+                    match char c with
+                    | 'r' -> '\r'
+                    | 'n' -> '\n'
+                    | c -> c
+                    |> buf.Append
+                    |> ignore
+                    aux()
+            | c ->
+                buf.Append c
+                |> ignore
+                aux()
+    aux()
+    let s = buf.ToString()
+    buf.Clear() |> ignore
+    readTrivia xs
+    s
 
 let readDateTime xs =
-    let y = readIntT xs
-    readCharT '/' xs
-    let M = readIntT xs
-    readCharT '/' xs
-    let d = readIntT xs
-    let h, m, s = readHms xs
-    try DateTime(y, M, d, h, m, s)
-    with
-    | :? ArgumentOutOfRangeException -> raiseReadExn "readDateTime"
+    match DateTime.TryParse(readStringLiteral xs) with
+    | true, x -> x
+    | _ -> raiseReadExn "readDateTime"
 
 let readTimeSpan xs =
-    try readHms xs |> TimeSpan
-    with
-    | :? ArgumentOutOfRangeException -> raiseReadExn "readTimeSpan"
+    match TimeSpan.TryParse(readStringLiteral xs) with
+    | true, x -> x
+    | _ -> raiseReadExn "readTimeSpan"
 
 let readEos xs = if xs.Index < xs.Items.Length then raiseReadExn "readEos"
 
@@ -112,8 +176,35 @@ let readAll r xs =
 
 let serializer() =
     let r xs = runString (readAll (readTuple3 readDateTime readTimeSpan readBool)) xs
-    let w x = sprintf "%A" x
+    let w (x: DateTime,y,z) = sprintf "(%A,%A,%A)" (x.ToUniversalTime().ToString("O")) (string y) z
     r, w
+
+let r, w = serializer()
+let test x =
+    let d = w x
+    let x' = r d 
+    printfn "w: %A, r: %A" d x'
+    x' = x
+
+let d = let t = DateTime.Now in (t, t.AddMinutes 20. - t, true)
+
+let (d1,d2,d3) = d
+let (d1',d2',d3') =
+    try w d |> r
+    with
+    | ReadException m ->
+        printfn "%s" m
+        reraise()
+
+
+DateTime.Now.ToUniversalTime().ToString("O")
+DateTime(calendar)
+
+d1.Ticks = d1'.Ticks
+d2 = d2'
+d3 = d3'
+
+test d
 
 let useSetting using =
     let path = @"C:\Users\Public\Document\Pallete.log"
